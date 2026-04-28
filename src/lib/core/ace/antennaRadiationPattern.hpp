6c9f1814fa97894af85887a074b2871327e0290b
R. van Twisk <github@rvt.dds.nl>
2026-04-28 18:43:17 +0200

    Improved flarm reception (#16)
    * Fix antenna pattern to be relative to aircraft
    * Track in FLARM if CRC corrections where performed

--- src/lib/core/ace/antennaRadiationPattern.hpp
+++ src/lib/core/ace/antennaRadiationPattern.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
+#include "coreutils.hpp"
 #include "messages.hpp"
 #include "etl/array.h"
-
 
 namespace GATAS
 {
@@ -11,23 +11,25 @@ namespace GATAS
      *
      * @tparam NUM_RADIALS
      */
-    template<size_t NUM_RADIALS = 8>
+    template <size_t NUM_RADIALS = 8>
     class AntennaRadiationPattern
     {
+    public:
         struct Measurement
         {
             int16_t avgRssiDbm;
-            int16_t maxRssiDbm; // the rssi that was received by maxDistance
+            int16_t maxRssiDbm;
             uint32_t avgDistance;
             uint32_t maxDistance;
+
             Measurement() : avgRssiDbm(-128), maxRssiDbm(-128), avgDistance(0), maxDistance(0)
             {
             }
 
-             Measurement(const Measurement&) = delete;
-             Measurement& operator=(const Measurement&) = delete;
-             Measurement(Measurement&&) = delete;
-             Measurement& operator=(Measurement&&) = delete;
+            Measurement(const Measurement &) = delete;
+            Measurement &operator=(const Measurement &) = delete;
+            Measurement(Measurement &&) = delete;
+            Measurement &operator=(Measurement &&) = delete;
         };
 
     private:
@@ -36,10 +38,12 @@ namespace GATAS
     public:
         AntennaRadiationPattern() : radiationPattern() {};
 
-
-        void put(const GATAS::IngressAircraftPositionMsg &msg) {
+        void put(const GATAS::IngressAircraftPositionMsg &msg)
+        {
             auto &position = msg.position;
-            uint8_t positionInRadial = CoreUtils::getRadialSection<NUM_RADIALS>(CoreUtils::bearingFromInDegShort(position.relEastFromOwn, position.relNorthFromOwn));
+            const float bearingFromOwn = CoreUtils::bearingFromInDegShort(position.relEastFromOwn, position.relNorthFromOwn);
+            const float relativeBearing = CoreUtils::toBearing(bearingFromOwn - static_cast<float>(position.track));
+            uint8_t positionInRadial = CoreUtils::getRadialSection<NUM_RADIALS>(relativeBearing);
             Measurement &measurement = radiationPattern[positionInRadial];
 
             if (position.distanceFromOwn > measurement.maxDistance)
@@ -51,26 +55,32 @@ namespace GATAS
             measurement.avgRssiDbm = (measurement.avgRssiDbm + msg.rssidBm) / 2;
         }
 
-        void serialize(etl::string_stream &stream) const {
-            stream << "[";
+        const etl::array<Measurement, NUM_RADIALS>& _radiationPattern() const
+        {
+            return radiationPattern;
+        }
 
-            // [ [avgRssiDbm, maxRssiDbm, avgDistance, maxDistance], [avgRssiDbm, maxRssiDbm, avgDistance, maxDistance], ....]
-            for (size_t i = 0; i < NUM_RADIALS; ++i) {
+        void serialize(etl::string_stream &stream) const
+        {
+            // Generates: [ [avgRssiDbm, maxRssiDbm, avgDistance, maxDistance], [avgRssiDbm, maxRssiDbm, avgDistance, maxDistance], ....]
+            stream << "[";
+            for (size_t i = 0; i < NUM_RADIALS; ++i)
+            {
                 const Measurement &m = radiationPattern[i];
 
                 stream << "[" << m.avgRssiDbm << ","
-                << m.maxRssiDbm << ","
-                << m.avgDistance << ","
-                << m.maxDistance << "]";
+                       << m.maxRssiDbm << ","
+                       << m.avgDistance << ","
+                       << m.maxDistance << "]";
 
-                if (i < NUM_RADIALS - 1) {
+                if (i < NUM_RADIALS - 1)
+                {
                     stream << ",";
                 }
             }
 
             stream << "]";
         }
-
     };
 
 }
